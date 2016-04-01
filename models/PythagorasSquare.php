034d26a5cf5f68c7d391c1a2cb2f1d62a84814d5
yslepianok <slepianok@gmail.com>
2016-04-01 13:28:55 +0300

    Simple PythagorasSquare counting

--- models/PythagorasSquare.php
+++ models/PythagorasSquare.php
@@ -15,6 +15,7 @@ class PythagorasSquare
     public $date = null;
     public $dateSymbols = [];
     public $workingChars = [];
+    public $workingCharsIntArray = [];
     public $simpleMatrix = [];
     public $extendedMatrix = [];
 
@@ -22,11 +23,12 @@ class PythagorasSquare
     {
         $this->date = $date->format('d-m-Y');
 
-        $this->dateSymbols = PythagorasSquare::formatIntoSymbolsArray($date);
+        $this->dateSymbols = self::formatIntoSymbolsArray($date);
         Yii::warning('Current date: '.$this->date);
-        $this->workingChars = PythagorasSquare::countWorkingChars($date);
-        $this->simpleMatrix = PythagorasSquare::countSquare($date);
-        $this->extendedMatrix = PythagorasSquare::countExtendedSquare($date);
+        $this->workingChars = self::countWorkingChars($date);
+        $this->workingCharsIntArray = self::explodeIntArrayIntoChars($this->workingChars);
+        $this->simpleMatrix = self::countSquare($date);
+        $this->extendedMatrix = self::countExtendedSquare($date);
     }
 
     // Creates array of symbols of this date
@@ -52,19 +54,71 @@ class PythagorasSquare
     public static function countWorkingChars(\DateTime $date)
     {
         $chars = [];
-        return [1,2,3,4];
+        $dateChars = self::formatIntoSymbolsArray($date);
+
+        //Первое рабочее число
+        $elem1 = 0;
+        foreach ($dateChars as $char) {
+            $elem1 += $char;
+        }
+
+        //Второе рабочее число
+        $str = (string)$elem1;
+        $elem2 = (integer)$str[0] + (integer)$str[1];
+
+        //Третье рабочее число
+        $elem3 = $elem1 - 2*(($dateChars[0]!==0) ? $dateChars[0] : $dateChars[1]);
+
+        //Четвертое рабочее число
+        $str = (string)$elem3;
+        $elem4 = (integer)$str[0] + (integer)$str[1];
+
+        return [$elem1,$elem2,$elem3,$elem4];
+    }
+
+    public static function explodeIntArrayIntoChars($chars)
+    {
+        $arr = [];
+        foreach ($chars as $char) {
+            $tempArray = str_split((string)$char);
+            foreach ($tempArray as $item) {
+                array_push($arr, $item);
+            }
+        }
+        Yii::warning('exploded working chars: '.implode(' ',$arr));
+        return $arr;
     }
 
     public static function countSquare(\DateTime $date)
     {
-        $workingChars = PythagorasSquare::countWorkingChars($date);
-        return [1,2,3,4,5,6,7,8,9];
+        $dateChars = self::formatIntoSymbolsArray($date);
+        $workingChars = self::explodeIntArrayIntoChars(self::countWorkingChars($date));
+        $square = [];
+
+        for ($i=1;$i<10;$i++)
+        {
+            $c = 0;
+            foreach ($workingChars as $char) {
+                if ($char==$i)
+                    $c++;
+            }
+            foreach ($dateChars as $char) {
+                if ($char==$i)
+                    $c++;
+            }
+            array_push($square,$c);
+        }
+
+        Yii::warning('date chars: '.implode(' ', $dateChars));
+        Yii::warning('working chars: '.implode(' ', $workingChars));
+        Yii::warning('square: '.implode(' ', $square));
+        return $square;
     }
 
     public static function countExtendedSquare(\DateTime $date)
     {
-        $workingChars = PythagorasSquare::countWorkingChars($date);
-        $initialSquare = PythagorasSquare::countSquare($date);
+        $workingChars = self::countWorkingChars($date);
+        $initialSquare = self::countSquare($date);
         return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
     }
 }
